2777d9bc65121125fe7380be77caffecd2925c48
ddochiii <s2018s29@e-mirim.hs.kr>
2019-12-22 08:15:32 +0900

    자료구조C언어

---
+++ DS/201.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define R 3
+int b[R] = { 0, };
+int cnt = 0;
+
+void PrintRePer() {
+	int i;
+	cnt++;
+	for (i = 0; i < R; i++)printf("  %d", b[i]);
+	printf("\n");
+}
+
+void RePermutation(int *a, int n, int r) {
+	int i;
+	if (r >= R)PrintRePer();
+	else for (i = 0; i < n; i++) {
+		b[r] = a[i];
+		RePermutation(a, n, r + 1);
+	}
+}
+
+int main() {
+	int a[] = { 1,2,3,4,5 };
+	int n = sizeof(a) / sizeof(int);
+
+	RePermutation(a, n, 0);
+	printf("중복 순열 개수 :%d", cnt);
+	return 0;
+}
---
+++ DS/207.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#define R 3
+int b[R] = { 0, };
+int cnt = 0;
+
+void PrintCom() {
+	int i;
+	cnt++;
+	for (i = 0; i < R; i++)printf("  %d", b[i]);
+	printf("\n");
+}
+
+void Swap(int *a, int *b) {
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void Combination(int *a, int n, int r, int ii) {
+	int i;
+	if(r >= R)PrintCom();
+	else for (i = ii + 1; i < n; i++) {
+	b[r] = a[i];
+	Combination(a, n, r + 1, i);
+	}
+}
+
+int main() {
+	int a[] = { 1,2,3,4,5 };
+	int n = sizeof(a) / sizeof(int);
+
+	Combination(a, n, 0, -1);
+	printf("조합 개수 :%d", cnt);
+	return 0;
+}
